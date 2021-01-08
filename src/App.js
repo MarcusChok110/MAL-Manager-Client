944fad563067a89a27009aa0377d0d4e712db56d
Marcus Chok <marcus.chok110@gmail.com>
2021-01-08 16:26:09 -0500

    Initial commit with create-react-app

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+  // const [data, setData] = useState('Empty');
+
+  // const getLogin = async () => {
+  //   const url = 'http://localhost:8888/';
+  //   const options = {
+  //     mode: 'cors',
+  //   };
+
+  //   const result = await fetch(url, options)
+  //     .then((response) => response.json())
+  //     .then((response) => {
+  //       //console.log(response);
+  //       return response;
+  //     });
+
+  //   setData(result);
+  // };
+
+  return (
+    <div>
+      <button>Login to MAL</button>
+    </div>
+  );
+}
+
+export default App;
