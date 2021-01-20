0c9b4a7b6632eba69e4e0defbd8ac0c4bb4687d0
Marcus Chok <marcus.chok110@gmail.com>
2021-01-19 23:24:02 -0500

    Add status radios and score select options to form

--- src/components/Anime.js
+++ src/components/Anime.js
@@ -1,11 +1,78 @@
 import React, { useEffect, useState } from 'react';
 
+const STATUS_ENUMS = {
+  Watching: 'watching',
+  Completed: 'completed',
+  'On Hold': 'on_hold',
+  Dropped: 'dropped',
+  'Plan To Watch': 'plan_to_watch',
+};
+
+const SCORE_ENUMS = {
+  'Select score': 0,
+  '(10) Masterpiece': 10,
+  '(9) Great': 9,
+  '(8) Very Good': 8,
+  '(7) Good': 7,
+  '(6) Fine': 6,
+  '(5) Average': 5,
+  '(4) Bad': 4,
+  '(3) Very Bad': 3,
+  '(2) Horrible': 2,
+  '(1) Appalling': 1,
+};
+
 const Anime = ({ match, animeList }) => {
   const [data, setData] = useState();
 
   useEffect(() => {
     fetchJSON();
   }, []);
+
+  // status radio buttons for form
+  const StatusRadio = ({ listData }) => {
+    const [statusData] = listData;
+    const radios = [];
+
+    for (let key in STATUS_ENUMS) {
+      radios.push(
+        <div className="form-check" key={radios.length}>
+          <input
+            className="form-check-input"
+            type="radio"
+            name="status"
+            value={STATUS_ENUMS[key]}
+            id={radios.length}
+            defaultChecked={
+              statusData
+                ? statusData.list_status.status === STATUS_ENUMS[key]
+                : false
+            }
+          />
+          <label className="form-check-label" htmlFor={radios.length}>
+            {key}
+          </label>
+        </div>
+      );
+    }
+
+    return radios;
+  };
+
+  // option select for score
+  const ScoreOptions = () => {
+    const options = [];
+
+    for (let key in SCORE_ENUMS) {
+      options.push(
+        <option value={SCORE_ENUMS[key]} key={options.length}>
+          {key}
+        </option>
+      );
+    }
+
+    return options;
+  };
 
   // form to update anime list status
   const ListForm = ({ list }) => {
@@ -14,7 +81,36 @@ const Anime = ({ match, animeList }) => {
     } else {
       const listData = inList(list);
       console.log(listData);
-      return <div>{JSON.stringify(listData)}</div>;
+      return (
+        <>
+          <form className="border rounded p-4" action="">
+            <fieldset className="form-group">
+              <div className="row">
+                <legend className="col-form-label col-2 pt-0">Status</legend>
+                <div className="col-10">
+                  <StatusRadio listData={listData} />
+                </div>
+              </div>
+            </fieldset>
+            <div className="form-group row">
+              <label
+                htmlFor="scoreSelect"
+                className="col-form-label col-2 pt-0"
+              >
+                Score
+              </label>
+              <select
+                name=""
+                id="scoreSelect"
+                className="form-control col-6 ml-2"
+                defaultValue={listData ? listData[0].list_status.score : 0}
+              >
+                <ScoreOptions />
+              </select>
+            </div>
+          </form>
+        </>
+      );
     }
   };
 
@@ -49,34 +145,34 @@ const Anime = ({ match, animeList }) => {
           <hr />
           <div className="row">
             <ul className="col-12 col-md-4 list-group list-group-flush">
-              <li class="list-group-item d-flex justify-content-between align-items-center">
+              <li className="list-group-item d-flex justify-content-between align-items-center">
                 <h5 className="lead">Score</h5>
                 <h5>
-                  <span class="">{data.score ? data.score : 'N/A'}</span>
+                  <span className="">{data.score ? data.score : 'N/A'}</span>
                 </h5>
               </li>
-              <li class="list-group-item d-flex justify-content-between align-items-center">
+              <li className="list-group-item d-flex justify-content-between align-items-center">
                 <h5 className="lead">Users Scored</h5>
                 <h5>
-                  <span class="">
+                  <span className="">
                     {data.scored_by ? data.scored_by.toLocaleString() : 'N/A'}
                   </span>
                 </h5>
               </li>
             </ul>
             <ul className="col-12 col-md-4 list-group list-group-flush">
-              <li class="list-group-item d-flex justify-content-between align-items-center">
+              <li className="list-group-item d-flex justify-content-between align-items-center">
                 <h5 className="lead">Ranked</h5>
                 <h5>
-                  <span class="">{`${
+                  <span className="">{`${
                     data.rank ? '#' + data.rank.toLocaleString() : 'N/A'
                   }`}</span>
                 </h5>
               </li>
-              <li class="list-group-item d-flex justify-content-between align-items-center">
+              <li className="list-group-item d-flex justify-content-between align-items-center">
                 <h5 className="lead">Popularity</h5>
                 <h5>
-                  <span class="">{`${
+                  <span className="">{`${
                     data.popularity
                       ? '#' + data.popularity.toLocaleString()
                       : 'N/A'
@@ -85,18 +181,18 @@ const Anime = ({ match, animeList }) => {
               </li>
             </ul>
             <ul className="col-12 col-md-4 list-group list-group-flush">
-              <li class="list-group-item d-flex justify-content-between align-items-center">
+              <li className="list-group-item d-flex justify-content-between align-items-center">
                 <h5 className="lead">Members</h5>
                 <h5>
-                  <span class="">
+                  <span className="">
                     {data.members ? data.members.toLocaleString() : 'N/A'}
                   </span>
                 </h5>
               </li>
-              <li class="list-group-item d-flex justify-content-between align-items-center">
+              <li className="list-group-item d-flex justify-content-between align-items-center">
                 <h5 className="lead">Favourites</h5>
                 <h5>
-                  <span class="">
+                  <span className="">
                     {data.favorites ? data.favorites.toLocaleString() : 'N/A'}
                   </span>
                 </h5>
@@ -114,7 +210,7 @@ const Anime = ({ match, animeList }) => {
                 </tr>
                 <tr>
                   <th scope="col">Episodes:</th>
-                  <td>{data.episodes}</td>
+                  <td>{data.episodes ? data.episodes : 'N/A'}</td>
                 </tr>
                 <tr>
                   <th scope="col">Status:</th>
@@ -155,7 +251,6 @@ const Anime = ({ match, animeList }) => {
     );
     const result = await response.json();
     setData(result);
-    console.log(result);
   };
 
   return (
