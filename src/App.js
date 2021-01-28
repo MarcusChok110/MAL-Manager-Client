0681d3d5c6ad84346fcc6830a91b79c1caa3cc8d
Marcus Chok <marcus.chok110@gmail.com>
2021-01-27 23:38:51 -0500

    Clean up dependencies

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App() {
 
   return (
     <div>
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <Navbar user={userToken} setUser={setUserToken} />
         <div className="container mt-2">
           <div className="App">
